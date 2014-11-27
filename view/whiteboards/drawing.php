96e96cd758a58ea33553363807b986dcad3a38fa
SanderBossuyt <sander.bossuyt@student.howest.be>
2014-11-27 18:44:59 +0100

    drawing.php upgedate

--- view/whiteboards/drawing.php
+++ view/whiteboards/drawing.php
@@ -7,12 +7,15 @@
     		if (!empty($items)) {
 				foreach($items as $item) {
 					if ($item['origin'] == "postit") {
+
 						echo "<div class=\"item postit {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
 							echo "<p>";
 							echo "{$item['content']}";
 							echo "</p>";
 						echo "</div>";
+
 					}else if ($item['origin'] == "image"){
+
 						echo "<div class=\"item picture {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
 						
                                 echo "<img draggable=\"true\" width=\"200\" height=\"200\" src=\"uploads/board/images/";
@@ -20,12 +23,15 @@
                                     
                                 echo "\"/>";
 						echo "</div>";
+
 					}else if($item['origin'] == "video"){
+
 						echo "<div class=\"item video {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
 						echo "<video width=\"320\" height=\"240\" controls>";
   						echo "<source src=\"uploads/board/videos/{$item['content']}\" type=\"video/mp4\">";
 						echo "</video>";
 						echo "</div>";
+						
 					}
 							
 				}
