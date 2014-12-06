f676b91ee1df64174eb96bb8f4ab4fdd35335b7b
GillesVDV <gilles.van.de.ven@hotmail.com>
2014-12-06 15:47:28 +0100

    leftbalk css

--- view/whiteboards/drawing.php
+++ view/whiteboards/drawing.php
@@ -1,10 +1,12 @@
 <div class="leftbalk">
 
-    <p> Board : <?php echo $board['name'] ?></p>
-    <p> aangemaakt op : <?php echo $board['creation_date'] ?></p>
-	
-	<form action="index.php?page=drawing&amp;id=<?php echo $_GET['id']; ?>" method="post" class="form-horizontal" id="formuploadpostit" enctype="multipart/form-data">
+    <p><span> Board : </span><?php echo $board['name'] ?></p>
+    <p><span> aangemaakt op : </span><?php echo $board['creation_date'] ?></p>
+    
+    <form action="index.php?page=drawing&amp;id=<?php echo $_GET['id']; ?>" method="post" class="form-horizontal" id="formuploadpostit" enctype="multipart/form-data">
             
+            <label>Postit:</label> 
+
             <div class="input text">
                 <label>
                     <input type="text" class="postitje" name="postit" value="<?php if(!empty($_POST['postit'])) echo $_POST['postit'];?>" placeholder="postit content" />
@@ -19,27 +21,30 @@
     </form>
 
 
+    <div class="dummy"></div>
 
 
       <form action="index.php?page=drawing&amp;id=<?php echo $_GET['id']; ?>" method="post" id="formuploadimage" enctype="multipart/form-data">
                 <div class="form-group<?php if(!empty($errors['image'])) echo ' has-error'; ?>">
-            <label for="addImageImage">Image:</label>     
-                <input type="file" name="image" id="addImageImage" class="form-control" value="<?php if(!empty($_POST['image'])) echo $_POST['image'];?>" />
-                </br>
-                <span class="error hidden"><?php
-                    if(empty($errors['image'])) echo 'Please select a square image';
-                    else echo $errors['image'];
-                ?>
-                </span>      
-        </div>
+                    <label for="addImageImage">Image:</label> <br/>    
+                    
+                    <input type="file" name="image" id="addImageImage" class="form-control" value="<?php if(!empty($_POST['image'])) echo $_POST['image'];?>" />
+                    </br>
+                    <span class="error hidden"><?php
+                        if(empty($errors['image'])) echo 'Please select a square image';
+                        else echo $errors['image'];
+                    ?>
+                    </span>      
+                </div>
+                
                 <input type="submit" name="action" value="upload image" class="uploadimage">
      </form>
 
 
+    <div class="dummy"></div>
 
 
-
-	<form action="index.php?page=drawing&amp;id=<?php echo $_GET['id']; ?>" method="post" class="form-horizontal" id="formuploadvideo" enctype="multipart/form-data">
+    <form action="index.php?page=drawing&amp;id=<?php echo $_GET['id']; ?>" method="post" class="form-horizontal" id="formuploadvideo" enctype="multipart/form-data">
             
             <div class="input-text">
                 
@@ -58,49 +63,53 @@
         
     </form>
 
-<a href="index.php?page=detail">go back</a>
+    <div class="dummy"></div>
+
+    <div id="back_button"><a href="index.php?page=detail">Go back</a></div>
+
 </div>
 
 
-<br />
+<br/>
 
 <div class="whiteboard">
 
-		<?php
-    		if (!empty($items)) {
-				foreach($items as $item) {
-					if ($item['origin'] == "postit") {
+        <?php
+            if (!empty($items)) {
+                foreach($items as $item) {
+                    if ($item['origin'] == "postit") {
 
-						echo "<div class=\"item postit {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
-							echo "<p>";
-							echo "{$item['content']}";
-							echo "</p>";
-						echo "</div>";
+                        echo "<div class=\"item postit {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
+                            echo "<p>";
+                            echo "{$item['content']}";
+                            echo "</p>";
+                        echo "</div>";
 
-					}else if ($item['origin'] == "image"){
+                    }else if ($item['origin'] == "image"){
 
-						echo "<div class=\"item picture {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
-						
+                        echo "<div class=\"item picture {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
+                        
                                 echo "<img draggable=\"true\" width=\"200\" height=\"200\" src=\"uploads/board/images/";
                                     echo $item['content'];
                                     
                                 echo "\"/>";
-						echo "</div>";
+                        echo "</div>";
 
-					}else if($item['origin'] == "video"){
+                    }else if($item['origin'] == "video"){
 
-						echo "<div class=\"item video {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
-						echo "<video width=\"320\" height=\"240\" controls>";
-  						echo "<source src=\"uploads/board/videos/{$item['content']}\" type=\"video/mp4\">";
-						echo "</video>";
-						echo "</div>";
-						
-					}
-							
-				}
-			}
-		?>
+                        echo "<div class=\"item video {$item['id']}\" style=\"left:{$item['x']}px; top:{$item['y']}px\">";
+                        echo "<video width=\"320\" height=\"240\" controls>";
+                        echo "<source src=\"uploads/board/videos/{$item['content']}\" type=\"video/mp4\">";
+                        echo "</video>";
+                        echo "</div>";
+                        
+                    }
+                            
+                }
+            }
+        ?>
 
 
 </div>
 <div id="achtergrond_whiteboard"></div>
+
